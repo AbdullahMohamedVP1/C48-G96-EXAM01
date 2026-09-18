bf028e7dc36e8783e3b9f30a5822b81c0e801675
AbdullahMohamedVP1 <boda12834@gmail.com>
2026-09-18 15:58:39 +0300

    Added Subject and Pract

---
+++ Exam_Project/PracticalExam.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exam_Project
+{
+    internal class PracticalExam : Exam
+    {
+        public override void ShowExam()
+        {
+        }
+    }
+}
---
+++ Exam_Project/Subject.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exam_Project
+{
+    internal class Subject{
+        public int SubjectId { get; set; }
+        public string SubjectName { get; set; }
+        public Exam Exam { get; set; }
+    }
+}
