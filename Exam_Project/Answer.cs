5b2e8bbd0c39f81b5576741a8bc2d99dd10b6e0d
AbdullahMohamedVP1 <boda12834@gmail.com>
2026-09-18 15:41:38 +0300

    making Question and Answers classes

---
+++ Exam_Project/Answer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exam_Project
+{
+    internal class Answer
+    {
+        public int AnswerId { get; set; }
+        public string AnswerText { get; set; }
+    }
+}
---
+++ Exam_Project/Question.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exam_Project
+{
+    internal class Question
+    {
+        public string Header { get; set; }
+        public string Body { get; set; }
+        public int Mark { get; set; }
+
+        public Answer[] Answers { get; set; }
+    }
+}
